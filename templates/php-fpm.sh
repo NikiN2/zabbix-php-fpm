4932166e6deff84cf64e871f283bac12d62b7826
NikiN2 <nikita@3093945.ru>
2016-05-26 12:16:35 +0300

    Update php-fpm.sh

--- templates/php-fpm.sh
+++ templates/php-fpm.sh
@@ -20,43 +20,43 @@ METRIC="$1"
 ## output metrics
 case $METRIC in
         pool)
-                cgi-fcgi -bind -connect 127.0.0.1:9000  | grep "^pool:"| cut -d ":" -f 2 | sed 's|^\s\+||'
+                /usr/bin/curl --max-time 20 --no-keepalive --silent "http://127.0.0.1:81/status"  | grep "^pool:"| cut -d ":" -f 2 | sed 's|^\s\+||'
                 ;;
         process_manager)
-                cgi-fcgi -bind -connect 127.0.0.1:9000 | grep "^process manager:"| cut -d ":" -f 2 | sed 's|^\s\+||'
+                /usr/bin/curl --max-time 20 --no-keepalive --silent "http://127.0.0.1:81/status" | grep "^process manager:"| cut -d ":" -f 2 | sed 's|^\s\+||'
                 ;;
         start_time)
-                cgi-fcgi -bind -connect 127.0.0.1:9000 | grep "^start time:"| sed 's|^start time:\s\+||'
+                /usr/bin/curl --max-time 20 --no-keepalive --silent "http://127.0.0.1:81/status" | grep "^start time:"| sed 's|^start time:\s\+||'
                 ;;
         start_since)
-                cgi-fcgi -bind -connect 127.0.0.1:9000 | grep "^start since:" | cut -d ":" -f 2 | sed 's|^\s\+||'
+                /usr/bin/curl --max-time 20 --no-keepalive --silent "http://127.0.0.1:81/status" | grep "^start since:" | cut -d ":" -f 2 | sed 's|^\s\+||'
                 ;;
         accepted_conn)
-                cgi-fcgi -bind -connect 127.0.0.1:9000 | grep "^accepted conn:" | cut -d ":" -f 2 | sed 's|^\s\+||'
+                /usr/bin/curl --max-time 20 --no-keepalive --silent "http://127.0.0.1:81/status" | grep "^accepted conn:" | cut -d ":" -f 2 | sed 's|^\s\+||'
                 ;;
         listen_queue)
-                cgi-fcgi -bind -connect 127.0.0.1:9000 | grep "^listen queue:" | cut -d ":" -f 2 | sed 's|^\s\+||'
+                /usr/bin/curl --max-time 20 --no-keepalive --silent "http://127.0.0.1:81/status" | grep "^listen queue:" | cut -d ":" -f 2 | sed 's|^\s\+||'
                 ;;
         max_listen_queue)
-                cgi-fcgi -bind -connect 127.0.0.1:9000 | grep "^max listen queue:" | cut -d ":" -f 2 | sed 's|^\s\+||'
+                /usr/bin/curl --max-time 20 --no-keepalive --silent "http://127.0.0.1:81/status" | grep "^max listen queue:" | cut -d ":" -f 2 | sed 's|^\s\+||'
                 ;;
         listen_queue_len)
-                cgi-fcgi -bind -connect 127.0.0.1:9000 | grep "^listen queue len:" | cut -d ":" -f 2 | sed 's|^\s\+||'
+                /usr/bin/curl --max-time 20 --no-keepalive --silent "http://127.0.0.1:81/status" | grep "^listen queue len:" | cut -d ":" -f 2 | sed 's|^\s\+||'
                 ;;
         idle_processes)
-                cgi-fcgi -bind -connect 127.0.0.1:9000 | grep "^idle processes:" | cut -d ":" -f 2 | sed 's|^\s\+||'
+                /usr/bin/curl --max-time 20 --no-keepalive --silent "http://127.0.0.1:81/status" | grep "^idle processes:" | cut -d ":" -f 2 | sed 's|^\s\+||'
                 ;;
         active_processes)
-                cgi-fcgi -bind -connect 127.0.0.1:9000 | grep "^active processes:" | cut -d ":" -f 2 | sed 's|^\s\+||'
+                /usr/bin/curl --max-time 20 --no-keepalive --silent "http://127.0.0.1:81/status" | grep "^active processes:" | cut -d ":" -f 2 | sed 's|^\s\+||'
                 ;;
         total_processes)
-                cgi-fcgi -bind -connect 127.0.0.1:9000 | grep "^total processes:" | cut -d ":" -f 2 | sed 's|^\s\+||'
+                /usr/bin/curl --max-time 20 --no-keepalive --silent "http://127.0.0.1:81/status" | grep "^total processes:" | cut -d ":" -f 2 | sed 's|^\s\+||'
                 ;;
         max_active_processes)
-                cgi-fcgi -bind -connect 127.0.0.1:9000 | grep "^max active processes:"  | cut -d ":" -f 2 | sed 's|^\s\+||'
+                /usr/bin/curl --max-time 20 --no-keepalive --silent "http://127.0.0.1:81/status" | grep "^max active processes:"  | cut -d ":" -f 2 | sed 's|^\s\+||'
                 ;;
         max_children_reached)
-                cgi-fcgi -bind -connect 127.0.0.1:9000 | grep "^max children reached:"  | cut -d ":" -f 2 | sed 's|^\s\+||'
+                /usr/bin/curl --max-time 20 --no-keepalive --silent "http://127.0.0.1:81/status" | grep "^max children reached:"  | cut -d ":" -f 2 | sed 's|^\s\+||'
                 ;;
         *)
                 echo "Unsupported metric $METRIC"
